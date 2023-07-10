6ae6ce377c9d4371ba9287523bd4cf572ec6475a
webdevlopment1245 <125587717+webdevlopment1245@users.noreply.github.com>
2023-07-10 11:40:07 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles.css";
+import DisplayAllPosts from "./DisplayAllPosts";
+
+
+const App = () => {
+  return (
+    <>
+      <DisplayAllPosts />
+    </>
+  );
+};
+
+export default App;
